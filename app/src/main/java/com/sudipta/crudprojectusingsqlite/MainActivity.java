e7447c8bc703952cd69b8fa25e9825ffbdb4d030
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-18 00:22:10 +0530

    Creating a contact object for the db and adding a contact to the db

--- app/src/main/java/com/sudipta/crudprojectusingsqlite/MainActivity.java
+++ app/src/main/java/com/sudipta/crudprojectusingsqlite/MainActivity.java
@@ -3,6 +3,10 @@ package com.sudipta.crudprojectusingsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.sudipta.crudprojectusingsqlite.data.MyDbHandeler;
+import com.sudipta.crudprojectusingsqlite.model.Contact;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyDbHandeler db = new MyDbHandeler(MainActivity.this);
+
+        //Creating a contact object for the db
+        Contact riju = new Contact();
+        riju.setName("Riju");
+        riju.setPhoneNumber("9090909090");
+
+        //adding a contact to he db
+        db.addContact(riju);
+
+        //Creating a contact object for the db
+        Contact leri = new Contact();
+        leri.setName("Leri");
+        leri.setPhoneNumber("6789709090");
+
+        //adding a contact to the db
+        db.addContact(leri);
+
+        //Creating a contact object for the db
+        Contact sudipta = new Contact();
+        sudipta.setName("Sudipta");
+        sudipta.setPhoneNumber("9087609090");
+
+        //adding a contact to he db
+        db.addContact(sudipta);
+
+        Log.d("DBRiju","name are: "+ riju.getName()+","+leri.getName()+","+sudipta.getName());
     }
 }
--- app/src/main/java/com/sudipta/crudprojectusingsqlite/data/MyDbHandeler.java
+++ app/src/main/java/com/sudipta/crudprojectusingsqlite/data/MyDbHandeler.java
@@ -1,12 +1,14 @@
 package com.sudipta.crudprojectusingsqlite.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import com.sudipta.crudprojectusingsqlite.model.Contact;
 import com.sudipta.crudprojectusingsqlite.params.Params;
 
-import androidx.annotation.Nullable;
 
 public class MyDbHandeler extends SQLiteOpenHelper {
 
@@ -16,11 +18,27 @@ public class MyDbHandeler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String create = "CREATE TABLE " + Params.TABLE_NAME + "("
+                + Params.KEY_ID + "INTEGER PRIMARY KEY,"
+                + Params.KEY_NAME + "TEXT,"
+                + Params.KEY_PHONE + "TEXT" + ")";
+        Log.d("DBRiju","Query being run is"+create);
+        db.execSQL(create);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Params.KEY_NAME, contact.getName());
+        values.put(Params.KEY_PHONE,contact.getPhoneNumber());
+
+        db.insert(Params.TABLE_NAME, null,values);
+        Log.d("DBRiju","successfull Inserted");
+        db.close();
     }
 }
